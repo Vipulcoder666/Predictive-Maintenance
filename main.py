2c7c97b321178521b25061b95f25254fc04a2dcf
Vipul Shrivastav <vipulmpgi@gmail.com>
2026-04-28 00:51:45 +0530

    First day

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = pd.read_csv("data.csv")
+
+avg = data["energy"].mean()
+
+print("Average energy:", avg)
+
+for i in data["energy"]:
+    if i > avg * 1.5:
+        print("⚠️ Alert: High energy usage detected")
